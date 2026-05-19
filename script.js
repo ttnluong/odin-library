1210fe0f79b3535ddb842f272da563d87f86c91d
Nhu <256359060+ttnluong@users.noreply.github.com>
2026-05-19 13:40:12 +0200

    Add JS function displayBooks

---
+++ script.js
@@ -0,0 +1,41 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+  const container = document.querySelector(".library");
+  container.innerHTML = "";
+
+  myLibrary.forEach(book => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.id = book.id;
+
+    card.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.read ? "Yes" : "No"}</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+
+addBookToLibrary("The Hobbit", "Tolkien", 310, true);
+addBookToLibrary("1984", "Orwell", 328, false);
+addBookToLibrary("Dune", "Herbert", 412, true);
+
+displayBooks();
